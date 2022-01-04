7bed3304c2c4dbd2bf2109cee280af125834398f
Mark Hildebrand <mhildebrand@ucdavis.edu>
2022-01-04 15:13:07 -0800

    Remove unused code

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,12 +13,6 @@ using Zygote
 
 const DATASET_DIR = joinpath(@__DIR__, "dataset")
 
-# # Embedding Table Lookup
-# include("embedding/constructors.jl")
-# include("embedding/lookup.jl")
-# include("embedding/map.jl")
-# include("embedding/update.jl")
-
 # Model Pipeline
 include("model/interact.jl")
 include("model/model.jl")
